178b175c8af37b5522d5a53be596d9aab74bdd7a
Thomas Klapprechner <thomas@laptop.de>
2022-02-18 22:02:18 +0100

    Unit-Test Struktur hinzugefügt; classpath wurde erweitert

---
+++ TestGithub/test/logic/ErsterTestfall.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package logic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author thomas
+ *
+ */
+class ErsterTestfall {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
